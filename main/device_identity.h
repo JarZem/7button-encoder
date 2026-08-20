82742f0df7401039f5a3e8733ce28026b5823850
JarZem <79940171+JarZem@users.noreply.github.com>
2026-08-20 23:35:07 +0200

    Expose current enrollment counter for challenge binding

--- main/device_identity.h
+++ main/device_identity.h
@@ -14,4 +14,5 @@ esp_err_t device_identity_get_device_id(char device_id[DEVICE_ID_MAX_LEN]);
 esp_err_t device_identity_get_public_key(uint8_t public_key[DEVICE_ENC_PUBLIC_KEY_LEN]);
 esp_err_t device_identity_get_public_key_fingerprint(uint8_t fingerprint[DEVICE_PUBLIC_KEY_FINGERPRINT_LEN]);
 uint16_t device_identity_get_key_id(void);
+esp_err_t device_identity_get_enrollment_counter(uint64_t *counter);
 esp_err_t device_identity_next_enrollment_counter(uint64_t *counter);
