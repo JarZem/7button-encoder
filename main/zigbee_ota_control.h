e1c6b0eb794445ac4b6c923d6923523274d6b17e
JarZem <79940171+JarZem@users.noreply.github.com>
2026-08-21 16:04:32 +0200

    Use bit flags for OTA status

--- main/zigbee_ota_control.h
+++ main/zigbee_ota_control.h
@@ -17,22 +17,43 @@ extern "C" {
 #define ZIGBEE_OTA_ENABLE_ATTR_ID             0x0000
 #define ZIGBEE_OTA_STATUS_ATTR_ID             0x0000
 
+/*
+ * OTA Status is a bit field:
+ *   bit7 ERROR
+ *   bit6 PROVISIONING
+ *   bit5 FIRMWARE
+ *   bit4 VERIFY
+ *   bit3 SKIPPED
+ *   bit2 TIMEOUT
+ *   bit1 FINISHED
+ *   bit0 STARTED
+ */
 typedef enum {
-    ZIGBEE_OTA_STATUS_IDLE                    = 0,
-    ZIGBEE_OTA_STATUS_PROVISIONING_STARTED    = 1,
-    ZIGBEE_OTA_STATUS_PROVISIONING_COMPLETE   = 2,
-    ZIGBEE_OTA_STATUS_FW_UPDATE_STARTED       = 16,
-    ZIGBEE_OTA_STATUS_FW_UPDATE_COMPLETE      = 17,
-    ZIGBEE_OTA_STATUS_PROVISIONING_ERROR      = 32,
-    ZIGBEE_OTA_STATUS_PROVISIONING_TIMEOUT    = 33,
-    ZIGBEE_OTA_STATUS_FW_UPDATE_ERROR         = 48,
-    ZIGBEE_OTA_STATUS_FW_VERIFY_ERROR         = 49,
-    ZIGBEE_OTA_STATUS_FW_SKIPPED              = 64,
+    ZIGBEE_OTA_STATUS_FLAG_ERROR               = 0x80,
+    ZIGBEE_OTA_STATUS_FLAG_PROVISIONING        = 0x40,
+    ZIGBEE_OTA_STATUS_FLAG_FIRMWARE            = 0x20,
+    ZIGBEE_OTA_STATUS_FLAG_VERIFY              = 0x10,
+    ZIGBEE_OTA_STATUS_FLAG_SKIPPED             = 0x08,
+    ZIGBEE_OTA_STATUS_FLAG_TIMEOUT             = 0x04,
+    ZIGBEE_OTA_STATUS_FLAG_FINISHED            = 0x02,
+    ZIGBEE_OTA_STATUS_FLAG_STARTED             = 0x01,
+
+    ZIGBEE_OTA_STATUS_IDLE                     = 0x00,
+    ZIGBEE_OTA_STATUS_PROVISIONING_STARTED     = 0x41,
+    ZIGBEE_OTA_STATUS_PROVISIONING_COMPLETE    = 0x42,
+    ZIGBEE_OTA_STATUS_PROVISIONING_ERROR       = 0xC2,
+    ZIGBEE_OTA_STATUS_PROVISIONING_TIMEOUT     = 0xC6,
+    ZIGBEE_OTA_STATUS_FW_UPDATE_STARTED        = 0x21,
+    ZIGBEE_OTA_STATUS_FW_UPDATE_COMPLETE       = 0x22,
+    ZIGBEE_OTA_STATUS_FW_UPDATE_ERROR          = 0xA2,
+    ZIGBEE_OTA_STATUS_FW_VERIFY_ERROR          = 0xB2,
+    ZIGBEE_OTA_STATUS_FW_SKIPPED               = 0x2A,
 } zigbee_ota_status_t;
 
 esp_err_t zigbee_ota_control_add_endpoint(esp_zb_ep_list_t *ep_list);
 bool zigbee_ota_control_handle_set_attr(const esp_zb_zcl_set_attr_value_message_t *message);
 bool zigbee_ota_control_is_enabled(void);
+void zigbee_ota_control_set_enabled(bool enabled);
 void zigbee_ota_control_set_status(zigbee_ota_status_t status);
 uint8_t zigbee_ota_control_get_status(void);
 
