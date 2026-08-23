f282c851cb1a87a11367820b025b8e7129bcf274
JarZem <79940171+JarZem@users.noreply.github.com>
2026-08-23 08:20:15 +0200

    Block status LED during OTA RF critical sections

--- main/jarzem_ota_project_hooks.c
+++ main/jarzem_ota_project_hooks.c
@@ -10,3 +10,13 @@ void jarzem_ota_hook_provision_step(void)
 {
     status_led_indicate_provision_step();
 }
+
+void jarzem_ota_hook_radio_critical_enter(void)
+{
+    status_led_block_rf_critical();
+}
+
+void jarzem_ota_hook_radio_critical_exit(void)
+{
+    status_led_unblock_rf_critical();
+}
