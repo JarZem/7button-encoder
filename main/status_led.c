93a6c41633183cbc9ecbaf5ed006aa0be6763f3b
JarZem <79940171+JarZem@users.noreply.github.com>
2026-08-23 16:51:14 +0200

    Keep provisioning LED packet-driven

--- main/status_led.c
+++ main/status_led.c
@@ -16,12 +16,6 @@ static bool ota_external_state(status_led_external_state_t *state, void *ctx)
 {
     (void)ctx;
     switch (ota_service_get_state()) {
-        case OTA_STATE_PENDING:
-            *state = (status_led_external_state_t){true, STATUS_LED_COLOR_ORANGE, 200, 85};
-            return true;
-        case OTA_STATE_CONNECTING_WIFI:
-            *state = (status_led_external_state_t){true, STATUS_LED_COLOR_CYAN, 200, 85};
-            return true;
         case OTA_STATE_DOWNLOADING:
             *state = (status_led_external_state_t){true, STATUS_LED_COLOR_WHITE, 200, 90};
             return true;
@@ -31,6 +25,8 @@ static bool ota_external_state(status_led_external_state_t *state, void *ctx)
         case OTA_STATE_FAILED:
             *state = (status_led_external_state_t){true, STATUS_LED_COLOR_RED, 200, 95};
             return true;
+        case OTA_STATE_PENDING:
+        case OTA_STATE_CONNECTING_WIFI:
         case OTA_STATE_SUCCESS:
         case OTA_STATE_IDLE:
         default:
@@ -91,19 +87,16 @@ void status_led_indicate_local_activity(void)
     status_led_manager_enqueue(STATUS_LED_COLOR_PURPLE, 200);
 }
 
-/* Any command/data received by the ESP from HA/Zigbee. */
 void status_led_indicate_ha_command(void)
 {
     status_led_manager_enqueue(STATUS_LED_COLOR_YELLOW, 200);
 }
 
-/* Any application data published/sent by the ESP. */
 void status_led_indicate_ha_publish(void)
 {
-    status_led_manager_enqueue(STATUS_LED_COLOR_CYAN, 200);
+    status_led_manager_enqueue(STATUS_LED_COLOR_CYAN, 150);
 }
 
-/* Provisioning frames are already indicated by the generic RX/TX indications. */
 void status_led_indicate_provision_step(void)
 {
 }
